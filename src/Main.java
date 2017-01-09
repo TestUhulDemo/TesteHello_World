be6134b2f3ac06b461457cf456e379e1a4ea6baa
Dado Note <borsa.dado@gmail.com>
2017-01-09 18:18:51 -0200

    Added secconf new Line

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ package src;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Nova Linha");
+        System.out.println("Nova Linha1");
+        System.out.println("Nova Linha2");
     }
 }
